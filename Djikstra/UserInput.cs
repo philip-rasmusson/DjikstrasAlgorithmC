2334269162e8c9ef2f6f2dcc5f587948f89993eb
Philip Rasmusson <rasmussonphilip@gmail.com>
2021-09-15 15:16:32 +0200

    Prints path in correct order. No wrong user input

--- Djikstra/UserInput.cs
+++ Djikstra/UserInput.cs
@@ -15,15 +15,13 @@ namespace Djikstra
             var whileLoop = true;
             var detour = false;
 
-
             for (int i = 0; i < nodes.Length; i++)
             {
                 Console.WriteLine($"Node {nodes[i]} = {i}");
             }
 
-
             Console.WriteLine("Select start node (number): ");
-            nodeQueue.Add(Backend.Invalid_input_check());
+            nodeQueue.Add(Backend.Invalid_input_check(0, Matrix.DefaultNodes.Length));
 
             while (whileLoop)
             {
@@ -40,16 +38,36 @@ namespace Djikstra
                     whileLoop = false;
                 }
             }
-
-            if(detour)
+            while(detour)
             {
                 Console.WriteLine("Select detour node (number): ");
-                nodeQueue.Add(Backend.Invalid_input_check());
+                var tempInput = (Backend.Invalid_input_check(0, Matrix.DefaultNodes.Length));
+                if (!nodeQueue.Contains(tempInput))
+                {
+                    nodeQueue.Add(tempInput);
+                    detour = false;
+                }
+                else
+                {
+                    Console.WriteLine("This node is already selected, please check another");
+                }
             }
+            whileLoop = true;
 
+            while (whileLoop)
+            {
             Console.WriteLine("Select end node (number): ");
-            nodeQueue.Add(Backend.Invalid_input_check());
-
+                var tempInput = (Backend.Invalid_input_check(0, Matrix.DefaultNodes.Length));
+                if (!nodeQueue.Contains(tempInput))
+                {
+                    nodeQueue.Add(tempInput);
+                    whileLoop = false;
+                }
+                else
+                {
+                    Console.WriteLine("This node is already selected, please check another");
+                }
+            }
             return nodeQueue;
         }
         
