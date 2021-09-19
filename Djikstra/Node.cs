b75364a6e12af4ac98813c18aba48229966329f1
Marcus Sjöholm <marcusasjoholm93@gmail.com>
2021-09-19 21:00:13 +0200

    First commit

--- Djikstra/Node.cs
+++ Djikstra/Node.cs
@@ -2,15 +2,15 @@
 {
     public class Node
     {
-        public char NodeA { get; set; }
-        public char NodeB { get; set; }
-        public int Edge { get; set; }
-
         public Node(char nodeA, char nodeB, int edge)
         {
             this.NodeA = nodeA;
             this.NodeB = nodeB;
             this.Edge = edge;
         }
+
+        public int Edge { get; set; }
+        public char NodeA { get; set; }
+        public char NodeB { get; set; }
     }
 }
