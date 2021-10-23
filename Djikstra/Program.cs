7034686d2d9bab4c85c518d20237259b002b6dcf
Philip <rasmussonphilip@gmail.com>
2021-10-23 08:26:50 +0200

    First commit on mac

--- Djikstra/Program.cs
+++ Djikstra/Program.cs
@@ -6,6 +6,7 @@
         {
             //Starts the program by asking for user input and calling the first method
             //with those values, as well as a starting value for total time (0 as default)
+            //
             Backend.RunMatrix(UserInput.GetUserInput(), 0);
         }
     }
